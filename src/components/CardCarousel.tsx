9d4c58b5233c3e8eb019798243ee3c0d7908375a
geroxima <geroshima123@gmail.com>
2024-10-05 22:56:50 -0400

    fix: refactor CardCarousel component and improve UI consistency

--- src/components/CardCarousel.tsx
+++ src/components/CardCarousel.tsx
@@ -6,8 +6,8 @@ import { Pagination } from 'swiper/modules';
 import data from '../data/facts.json';
 import '../CardCarousel.css';
 
-// Define the type for the item
 interface CarouselItem {
+  title: string;
   text: string;
   image?: string;
 }
@@ -24,7 +24,8 @@ const CardCarousel = () => {
       {data.map((item: CarouselItem, index: number) => (
         <SwiperSlide key={index}>
           <div className="swiper-slide-content gap-y-10">
-            <p>{item.text}</p>
+            <h1 className='text-6xl font-bold tracking-tight text-zinc-600'>{item.title}</h1>
+            <p className='text-3xl text-zinc-900 font-normal tracking-tighter text-pretty leading-relaxed'>{item.text}</p>
             {item.image && <img src={item.image} alt={`Slide ${index}`} className='rounded-lg w-[30rem]' />}
 
           </div>

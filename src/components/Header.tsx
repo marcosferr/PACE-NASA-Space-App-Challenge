2a6c61fac1f844a21fd63af5076c08c89c5c1e4e
geroxima <geroshima123@gmail.com>
2024-10-06 17:42:28 -0300

    refactor: update favicon and page title
    Update the favicon and page title to reflect the ARANDU PACE - NASA SPACE APPS CHALLENGUE 2024 branding.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,10 @@ const Header = () => {
 
     return (
         <header className="flex py-4 px-8 justify-between">
-            <a href="\">
-                <h1 className="text-sm font-bold text-green-600">ARANDU PACE</h1>
-            </a>
+            <a href="/" className="flex gap-x-4 items-center">
+                <img src="./arandu-pace-icon.png" alt="" className="size-7"/>
+                <img src='./arandu-logo.webp' alt='Arandu logo' className="h-7"/>
+                </a>
         </header>
     );
 };
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,15 +5,14 @@ const LandingPage = () => {
     return (
         <main>
             <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#7dc9aa_100%)]"></div>
-            <Header />
 
-            <section className='flex flex-col items-center gap-y-10 px-8 xl:px-0'>
+            <section className='flex flex-col items-center gap-y-10 px-8 xl:px-0 mt-10'>
+                <img src="./arandu-pace-icon.png" alt="arandu pace icon" />
                 <div className='flex flex-col items-center gap-y-2'>
-                    <h1 className="text-4xl xl:text-7xl  font-black tracking-tight  text-green-700">ARANDU PACE</h1>
+                   <img src='./arandu-logo.webp' alt='Arandu logo' />
                     <p className='font-normal'> Arandu means <i>the wisdom rooted in nature</i></p>
-                    <h2 className='text-normal xl:text-2xl font-normal text-green-950'>Plankton, Aerosol, Cloud, ocean Ecosystem </h2>
                 </div>
-                <img src="./pace-space.webp" alt="A picture of the pace spacecraft" className='rounded-lg w-full  lg:w-1/3 xl:w-1/3' />
+                {/* <img src="./pace-space.webp" alt="A picture of the pace spacecraft" className='rounded-lg w-full  lg:w-1/3 xl:w-1/3' /> */}
                     <a href="/app" className='bg-zinc-950 border border-green-950 text-white rounded-md w-full lg:w-1/3 xl:w-1/3 text-center py-2 hover:brightness-150 transition-all ease-in-out duration-300'>✨ Start Learning</a>
                 <p className="text-center text-green-900 text-sm font-normal max-w-lg mt-4">
                     Unlock the power of scientific knowledge through the PACE mission—understanding the ocean and atmosphere to sustain life on Earth.

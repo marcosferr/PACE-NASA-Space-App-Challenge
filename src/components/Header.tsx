6ce0dbdfc680cd0d4b959a19d37e7bbd7bc31998
geroxima <geroshima123@gmail.com>
2024-10-06 09:36:40 -0300

    feat: Add react-router-dom package for routing functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,6 @@ const Header = () => {
     return (
         <header className="flex py-4 px-8 justify-between">
             <h1 className="text-sm font-bold text-green-600">ARANDU PACE</h1>
-            <div className="underline text-sm font-semibold">
-                SELECCIONAR IDIOMA
-            </div>
         </header>
     );
 };
---
+++ src/components/icons/SideBarCollapse.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function SideBarCollapseIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      strokeWidth="1.5"
+      color="#000"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19 21H5a2 2 0 01-2-2V5a2 2 0 012-2h14a2 2 0 012 2v14a2 2 0 01-2 2z"
+      ></path>
+      <path
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M7.25 10L5.5 12l1.75 2M9.5 21V3"
+      ></path>
+    </svg>
+  );
+}
+
+export default SideBarCollapseIcon;
